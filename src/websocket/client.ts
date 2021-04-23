d04b6a4723fe79f06417f1040bf1fd3d46916910
Denis Batista <denis.si2011@gmail.com>
2021-04-23 20:25:37 -0300

    Finalizando aplicação

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -2,13 +2,13 @@ import { io } from "../http";
 import { ConnectionsServices } from "../services/ConnectionsServices";
 import { UsersServices } from "../services/UsersServices";
 import { MessagesServices } from "../services/MessagesServices";
+import { Socket } from "socket.io";
 
 
 interface IParams {
     text: string;
     email: string;
 }
-
 
 io.on("connect", (socket) => {
     const connectionsService = new ConnectionsServices();
@@ -17,7 +17,7 @@ io.on("connect", (socket) => {
 
     socket.on("client_first_access", async (params) => {
         const socket_id = socket.id;
-        const { text, email } = params;
+        const { text, email } = params as IParams;
         let user_id = null;
 
 
@@ -59,6 +59,35 @@ io.on("connect", (socket) => {
         await messagesService.create({
             text,
             user_id
+        });
+
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit("client_list_all_messages", allMessages);
+
+        const allUsers = await connectionsService.findAllWithoutAdmin();
+        io.emit("admin_list_all_users", allUsers);
+
+    });
+
+
+
+    socket.on("client_send_to_admin", async (params) => {
+
+        const { text, socket_admin_id } = params;
+
+        const socket_id = socket.id;
+
+        const { user_id } = await connectionsService.findBySocketID(socket_id);
+
+        const message = await messagesService.create({
+            text,
+            user_id,
+        });
+
+        io.to(socket_admin_id).emit("admin_receive_message", {
+            message,
+            socket_id
         });
 
     });
